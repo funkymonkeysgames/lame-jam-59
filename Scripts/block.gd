423c95720abf00377cc8f89f25ea485f3ecbfa3d
umutucak <umutucak@proton.me>
2026-02-20 16:43:25 +0100

    neighbour check only upon object collision + confirm button QoL

--- Scripts/block.gd
+++ Scripts/block.gd
@@ -2,14 +2,17 @@ extends CharacterBody2D
 
 var can_drag = false
 var drag_offset = Vector2(0,0)
-var integrity = 0
-var force = Vector2.ZERO
 
-@onready var neighborArea2D = $Neighbor
+var integrity: int
+
+@onready var neighbour_area2d: Area2D = $Neighbour
+@onready var parent_body2d = $".."
+@onready var integrity_text: TextEdit = $IntegrityText
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	integrity = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -21,10 +24,7 @@ func _physics_process(delta: float) -> void:
 		var speed = distance / delta
 		velocity = direction * speed
 		move_and_slide()
-		
-func _process(delta: float) -> void:
-	update_integrity()
-	print(integrity)
+
 
 func _on_button_button_down() -> void:
 	drag_offset = get_global_mouse_position() - global_position
@@ -32,12 +32,11 @@ func _on_button_button_down() -> void:
 
 func _on_button_button_up() -> void:
 	can_drag = false
-	
-func update_integrity():
-	var neighbors = neighborArea2D.get_overlapping_areas()
-	if (neighbors.size() > 0):
-		var max_integrity = 0
-		for i in neighbors.size():
-			max_integrity = max(max_integrity, neighbors[i].body.integrity)
-			
-		integrity = max_integrity - 1
+
+
+func _on_neighbor_area_entered(area: Area2D) -> void:
+	var max_neighbour_integrity = 0
+	for neighbour: Area2D in neighbour_area2d.get_overlapping_areas():
+		max_neighbour_integrity = max(max_neighbour_integrity, neighbour.get_parent().integrity)
+	integrity = max_neighbour_integrity - 1
+	integrity_text.text = str(integrity)
