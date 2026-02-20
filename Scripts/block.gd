005b4deff15e449259d9627dae885105b8c67d6a
umutucak <umutucak@proton.me>
2026-02-20 15:28:51 +0100

    implemented updating integrity based on the neighbors integrity

--- Scripts/block.gd
+++ Scripts/block.gd
@@ -5,6 +5,8 @@ var drag_offset = Vector2(0,0)
 var integrity = 0
 var force = Vector2.ZERO
 
+@onready var neighborArea2D = $Neighbor
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,6 +21,10 @@ func _physics_process(delta: float) -> void:
 		var speed = distance / delta
 		velocity = direction * speed
 		move_and_slide()
+		
+func _process(delta: float) -> void:
+	update_integrity()
+	print(integrity)
 
 func _on_button_button_down() -> void:
 	drag_offset = get_global_mouse_position() - global_position
@@ -26,3 +32,13 @@ func _on_button_button_down() -> void:
 
 func _on_button_button_up() -> void:
 	can_drag = false
+	
+func update_integrity():
+	var neighbors = neighborArea2D.get_overlapping_areas()
+	if (neighbors.size() > 0):
+		var max_integrity = 0
+		for i in neighbors.size():
+			max_integrity = max(max_integrity, neighbors[i].body.integrity)
+			
+		integrity = max_integrity - 1
+	
--- Scripts/neighbour.gd
+++ Scripts/neighbour.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var body = $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
