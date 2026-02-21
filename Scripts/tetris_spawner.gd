e8057e4834d796d81576bbaeef6d532f6a31951f
umutucak <umutucak@proton.me>
2026-02-21 21:44:24 +0100

    remove old block scene

--- Scripts/tetris_spawner.gd
+++ Scripts/tetris_spawner.gd
@@ -19,7 +19,6 @@ var soda_can_scene = preload("res://Scenes/Objects/soda_can.tscn")
 var spray_can_scene = preload("res://Scenes/Objects/spray_can.tscn")
 var styro_cup_scene = preload("res://Scenes/Objects/styro_cup.tscn")
 var tin_can_scene = preload("res://Scenes/Objects/tin_can.tscn")
-var block_scene = preload("res://Scenes/block_example.tscn")
 
 var currentObject: Node = null
 var nextObject: Node = null
@@ -55,7 +54,6 @@ func initilizeSceneArray() -> void:
 	possible_objects.append(spray_can_scene)
 	possible_objects.append(styro_cup_scene)
 	possible_objects.append(tin_can_scene)
-	#possible_objects.append(block_scene)
 
 func setObjects():
 	if nextObject == null:
