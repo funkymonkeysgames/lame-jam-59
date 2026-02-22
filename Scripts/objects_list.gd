918b2fce02219fa1f147183cdf3f787f3b39a5f3
umutucak <umutucak@proton.me>
2026-02-22 14:30:54 +0100

    sfx added back

--- Scripts/objects_list.gd
+++ Scripts/objects_list.gd
@@ -20,25 +20,15 @@ func _on_child_entered_tree(node: Block) -> void:
 	node.neighbour_entered.connect(_on_neighbour_entered)
 	
 func _on_block_placed() -> void:
+	print("placed")
 	audio_streams.get_node("Success").play()
 	
 func _on_block_missed() -> void:
-	audio_streams.get_node("Rejection").play()
+	audio_streams.get_node("Fail").play()
 
 func _on_block_pickedup() -> void:
-	audio_streams.get_node("Pickup").play()
+	pass
+	#audio_streams.get_node("Pickup").play()
 
 func _on_neighbour_entered(integrity: int) -> void:
-	print(integrity)
-	if integrity == 0:
-		audio_streams.get_node("Integrity0").play()
-	elif integrity == 1:
-		audio_streams.get_node("Integrity1").play()
-	elif integrity == 2:
-		audio_streams.get_node("Integrity2").play()
-	elif integrity == 3:
-		audio_streams.get_node("Integrity3").play()
-	elif integrity == 4:
-		audio_streams.get_node("Integrity4").play()
-	elif integrity == 5:
-		audio_streams.get_node("Integrity5").play()
+	audio_streams.get_node("Contact").play()
